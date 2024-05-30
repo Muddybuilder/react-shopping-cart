f7ade79791c0b352b7fd1edce01d4ab5c0ec69b7
Hoon Jeong <yong08211@gmail.com>
2024-05-29 22:14:27 -0400

    add todo

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,10 @@
+import { useState } from "react";
 import Header from "./Header";
 import { Outlet } from "react-router-dom";
 
 export default function App() {
+  const [cartItem, setCartItem] = useState([]);
+  //TODO: Propagate cartItem, setCartItem to children components
   return (
     <>
       <Header />
